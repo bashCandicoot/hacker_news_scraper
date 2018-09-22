cbacea362044bd80492c712643103f23671e7799
bashCandicoot <ste7@protonmail.com>
2018-09-22 15:01:43 +0100

    get content for x amount of posts

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-console.log('init');
+const Scraper = require('./Scraper');
+const parseArgs = require('minimist');
 
+const argv = parseArgs(process.argv.slice(2));
+const scraper = new Scraper({ argv });
+
+async function main() {
+  const topPostIds = await scraper.getTopPostIds();
+  const postIds = scraper.getTopPostsSubset(topPostIds);
+  const posts = await scraper.attachContentToPosts(postIds);
+
+  const pickedFields = Scraper.pickFields(posts);
+  const mappedPosts = Scraper.mapPostKeys(pickedFields);
+  console.log(mappedPosts);
+}
+
+
+main();
